b621934cbd73a05db39a6a26d837c105f8f3e088
BukkyOmo <odunayobukky1@gmail.com>
2019-05-06 17:35:50 +0100

    165763034: task(frontend)- Setup react router
    - [x] npm install react-router-dom
    - [x] create a routes folder
    - [x] include an index.js and add routes
    - [x] use BrowserRouter, Link, Switch to navigate
    
    [Delivers #165763034]

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Routes from './routes/index';
+
+const App = () => (
+  <Routes />
+);
+
+export default App;
--- webpack.common.js
+++ webpack.common.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src', 'index.jsx'),
+  entry: path.resolve(__dirname, 'src', 'index.js'),
   module: {
     rules: [
       {
